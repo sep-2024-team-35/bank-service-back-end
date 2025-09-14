9d135f76da06ce8144dac7303dbc81d56c125823
Uslje <147374020+uslje13@users.noreply.github.com>
2025-09-14 10:51:09 +0200

    refactor: transactional save request

--- services/payment-service.go
+++ services/payment-service.go
@@ -6,6 +6,9 @@ import (
 	"github.com/sep-2024-team-35/bank-servce-back-end/dto"
 	"github.com/sep-2024-team-35/bank-servce-back-end/models"
 	"github.com/sep-2024-team-35/bank-servce-back-end/repositories"
+	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
 type PaymentService interface {
@@ -13,19 +16,21 @@ type PaymentService interface {
 }
 
 type paymentService struct {
-	repo           repositories.PaymentRepository
-	accountService AccountService
+	paymentRepository     repositories.PaymentRepository
+	transactionRepository repositories.TransactionRepository
+	accountService        AccountService
 }
 
-func NewPaymentService(repo repositories.PaymentRepository, accSvc AccountService) PaymentService {
+func NewPaymentService(repo repositories.PaymentRepository, accSvc AccountService, transactionRepository repositories.TransactionRepository) PaymentService {
 	return &paymentService{
-		repo:           repo,
-		accountService: accSvc,
+		paymentRepository:     repo,
+		accountService:        accSvc,
+		transactionRepository: transactionRepository,
 	}
 }
 
-func (s *paymentService) CreateRequest(dto dto.PaymentRequestDto) (*models.PaymentRequest, error) {
-	exists, err := s.accountService.isMerchantAccountExisting(dto.MerchantId, dto.MerchantPassword)
+func (s *paymentService) CreateRequest(requestDto dto.PaymentRequestDto) (*models.PaymentRequest, error) {
+	exists, err := s.accountService.isMerchantAccountExisting(requestDto.MerchantID, requestDto.MerchantPassword)
 	if err != nil {
 		return nil, err
 	}
@@ -35,15 +40,51 @@ func (s *paymentService) CreateRequest(dto dto.PaymentRequestDto) (*models.Payme
 
 	request := &models.PaymentRequest{
 		ID:                uuid.New(),
-		MerchantID:        dto.MerchantId,
-		MerchantPassword:  dto.MerchantPassword,
-		Amount:            dto.Amount,
-		MerchantOrderID:   dto.MerchantOrderId,
-		MerchantTimestamp: dto.MerchantTimestamp,
-		SuccessURL:        dto.SuccessUrl,
-		FailedURL:         dto.FailedUrl,
-		ErrorURL:          dto.ErrorUrl,
+		MerchantID:        requestDto.MerchantID,
+		MerchantPassword:  requestDto.MerchantPassword,
+		Amount:            requestDto.Amount,
+		MerchantOrderID:   requestDto.MerchantOrderId,
+		MerchantTimestamp: requestDto.MerchantTimestamp,
+		SuccessURL:        requestDto.SuccessUrl,
+		FailedURL:         requestDto.FailedUrl,
+		ErrorURL:          requestDto.ErrorUrl,
 	}
 
-	return s.repo.SaveRequest(request)
+	var savedRequest *models.PaymentRequest
+
+	err = s.paymentRepository.DB().Transaction(func(tx *gorm.DB) error {
+		var err error
+
+		savedRequest, err = s.paymentRepository.SaveRequestTransactional(tx, request)
+		if err != nil {
+			return err
+		}
+
+		newTransaction := &models.Transaction{
+			Amount:            savedRequest.Amount.InexactFloat64(),
+			MerchantOrderID:   savedRequest.MerchantOrderID,
+			MerchantTimestamp: savedRequest.MerchantTimestamp.Format(time.RFC3339),
+			AcquirerOrderID:   generateRandomOrderID(),
+			AcquirerTimestamp: time.Now().Format(time.RFC3339),
+			IssuerOrderID:     "", // TODO: implement
+			IssuerTimestamp:   "", // TODO: implement
+			Status:            "CREATED",
+			PaymentRequestID:  request.ID,
+		}
+
+		_, err = s.transactionRepository.SaveTransactional(tx, newTransaction)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] Failed to create payment request and transaction for MerchantID=%s: %v", requestDto.MerchantID, err)
+		return nil, err
+	}
+
+	log.Printf("[INFO] Payment request and transaction successfully created: ID=%s", savedRequest.ID.String())
+	return savedRequest, nil
 }
